2e1a990f7fd63c5dfe21d0afc77502b3287fa715
rodiyansyah <galuh29@gmail.com>
2020-05-20 10:48:35 +0700

    Tutorial

--- tutorial 3.py
+++ tutorial 3.py
@@ -13,5 +13,32 @@ titanic = pd.read_csv('titanic.csv')
 # print(titanic.head())
 
 #ambil seluruh kolom Age
+print(titanic['Age'].head())
 
-print(titanic['Age'])
+print('type titanic[\'age\']: ',type(titanic['Age']))
+
+print('shape titanic[\'age\']: ',titanic['Age'].shape)
+
+age_sex = titanic[['Age', 'Sex']]
+
+print(age_sex.head())
+
+print('type titanic[\'age\',\'sex\'] : ',type(age_sex))
+
+print('shape titanic[\'age\',\'sex\'] : ',age_sex.shape)
+
+print('==========================')
+above35 = titanic[titanic['Age']>35]
+print('diatas 35')
+print(above35)
+
+class23 = titanic[titanic['Pclass'].isin([2,3])]
+print('class 2 dan 3')
+print(class23)
+
+
+age_no_na = titanic[titanic['Age'].notna()]
+print('ada data umurnya')
+print(age_no_na)
+
+
